ff54cd2a3344ed3f1fbb7b89987d5e137f34faac
krushil-26 <108085307+krushil-26@users.noreply.github.com>
2023-04-17 17:57:20 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+export default {
+    getData: () => axios ({
+        method: 'GET',
+        url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities',
+        params: {countryIds: 'IN', namePrefix: 'del', limit: '3'},
+        headers: {
+        'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com',
+        'x-rapidapi-key': '4ac5e3352fmshe6ac515ca3b8ccap1f0045jsnf0a504a87bbe'
+        }
+    })
+  };
